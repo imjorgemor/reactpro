5698ef80cbcc222676aa6744fb29ca7b7d06243a
Jorge Mor <imgeorgemore@icloud.com>
2022-05-08 20:55:36 +0200

    Set up forms branch

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
+import RegisterPage from "../forms/pages/RegisterPage";
 import logo from '../logo.svg';
 
 const Navigation = () => {
@@ -9,13 +10,16 @@ const Navigation = () => {
                     <img src={logo} alt="React logo" />
                     <ul>
                         <li>
-                            <NavLink to="/home" className={({ isActive }) => isActive ? "nav-active" : ""}>Home</NavLink>
+                            <NavLink to="/" className={({ isActive }) => isActive ? "nav-active" : ""}>Home</NavLink>
                         </li>
                         <li>
                             <NavLink to="/about" className={({ isActive }) => isActive ? "nav-active" : ""}>About</NavLink>
                         </li>
                         <li>
                             <NavLink to="/users" className={({ isActive }) => isActive ? "nav-active" : ""}>Users</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/register" className={({ isActive }) => isActive ? "nav-active" : ""}>Register - Forms</NavLink>
                         </li>
                     </ul>
                 </nav>
@@ -24,7 +28,7 @@ const Navigation = () => {
                     <Route path="/" element={<h1>home</h1>} />
                     <Route path="/about" element={<h1>About Page</h1>} />
                     <Route path="/users" element={<h1>Users Page</h1>} />
-                    <Route path="/*" element={<h2>not found</h2>} />
+                    <Route path="/register" element={<RegisterPage />} />
                 </Routes>
             </div>
         </BrowserRouter>
